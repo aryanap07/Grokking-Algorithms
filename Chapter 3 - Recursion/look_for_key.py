c25bf0551ee5a8b015dda3bcaf1fb2b65fe1daa3
Aryan <proaryanpatel2906@gmail.com>
2026-06-11 20:38:16 +0530

    Grandma's Attic

--- Chapter 3 - Recursion/look_for_key.py
+++ Chapter 3 - Recursion/look_for_key.py
@@ -3,11 +3,11 @@ def look_for_key(main_box):
     pile = main_box.make_a_pile_to_look_through()
     while pile is not empty:
         box = pile.grab_a_box()
-    for item in box:
-        if item.is_a_box():
-            pile.append(item)
-        elif item.is_a_key():
-            print("Found the Key!")
+        for item in box:
+            if item.is_a_box():
+                pile.append(item)
+            elif item.is_a_key():
+                print("Found the Key!")
 
 #Second Approch (Recusion)
 def look_for_key(main_box):
